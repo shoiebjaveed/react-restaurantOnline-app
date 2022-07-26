28eab45c79cf5a8c737f33e9e7865e0df6c32f14
shoiebjaveed <shoiebjaveed@gmail.com>
2022-07-27 01:34:07 +0530

    Working cart items

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,18 +1,18 @@
 import { Fragment } from 'react';
 
 import HeaderCartButton from './HeaderCartButton';
-import headerImage from '../../assets/meals.jpg';
+import mealsImage from '../../assets/meals.jpg';
 import classes from './Header.module.css';
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>Today's Meals</h1>
+        <h1>ReactMeals</h1>
         <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes['main-image']}>
-        <img src={headerImage} alt='delicious food right into your home!' />
+        <img src={mealsImage} alt='A table full of delicious food!' />
       </div>
     </Fragment>
   );
